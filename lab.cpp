28c176e21372be2e2627eb80c0a9a3da4df96b21
lettergram <lettergram@gmail.com>
2014-04-25 09:35:57 -0500

    Converts RGB images to the lab space, using openCV

---
+++ lab.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include <opencv/cv.h>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv/highgui.h>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/opencv.hpp>
+
+/* version 2.4 of openCV */
+using namespace cv;
+
+Mat BGR2LAB(Mat const &imgRGB){
+
+	Mat imgLab;
+	cvtColor(imgRGB, imgLab, CV_RGB2Lab);
+
+	return imgLab;
+}
+
+Mat alphaLAB(Mat const &imgLab){
+
+	Mat alphaLab;
+	cvtColor(imgLab, alphaLab, CV_Lab2RGB);
+	Size s = alphaLab.size();
+
+	int steps = imgLab.step;
+	int channels = imgLab.channels();
+
+	for(int y = 0; y < s.height; y++){
+		for(int x = 0; x < s.width; x++){
+			(*alphaLab.ptr<Point3_<uchar> >(y,x)).x = imgLab.data[steps*y + channels * x + 1];
+			(*alphaLab.ptr<Point3_<uchar> >(y,x)).y = imgLab.data[steps*y + channels * x + 1];
+			(*alphaLab.ptr<Point3_<uchar> >(y,x)).z = imgLab.data[steps*y + channels * x + 1];
+		}
+	}
+	return alphaLab;
+}
+
+Mat betaLAB(Mat &imgLab){
+	Mat betaLab;
+	cvtColor(imgLab, betaLab, CV_Lab2RGB);
+	Size s = betaLab.size();
+	int steps = imgLab.step;
+	int channels = imgLab.channels();
+	for(int y = 0; y < s.height; y++){
+		for(int x = 0; x < s.width; x++){
+			(*betaLab.ptr<Point3_<uchar> >(y,x)).x = imgLab.data[steps*y + channels * x + 2];
+			(*betaLab.ptr<Point3_<uchar> >(y,x)).y = imgLab.data[steps*y + channels * x + 2];
+			(*betaLab.ptr<Point3_<uchar> >(y,x)).z = imgLab.data[steps*y + channels * x + 2];
+		}
+	}
+	return betaLab;
+}
+
+int main(int argc, const char* argv[]){
+
+    Mat imgLab =  BGR2LAB(imread(argv[1], CV_LOAD_IMAGE_COLOR)); 
+
+	char filename[60];
+
+	sprintf(filename, "output/%s-rgb_2_lab.jpg", argv[1]);
+    imwrite(filename, imgLab);
+	sprintf(filename, "output/%s-alphaLab.jpg", argv[1]);
+	imwrite(filename, alphaLAB(imgLab));
+	sprintf(filename, "output/%s-betaLab.jpg", argv[1]);
+	imwrite(filename, betaLAB(imgLab));
+
+    return 0;
+}
